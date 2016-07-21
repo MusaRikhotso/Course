dd9fa35908aa417d339bfdafc9223e4060806179
MusaRikhotso <1090294@students.wits.ac.za>
2016-07-21 23:00:15 +0200

    Almost done

--- hw0/main.c
+++ hw0/main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <sys/time.h>
-#include <sys/resource.h>
+#include <sys/resource.h> 
 
 int main() {
     struct rlimit lim;
     // print the following values using rlimit
-    printf("stack size: (max limit) %ld \n", lim.rlim_max);
-    printf("process limit: (The maximum stack size for the process) %d\n", RLIMIT_STACK);
-    printf("max file descriptors: (current limit) %ld\n", lim.rlim_cur);
+     getrlimit(RLIMIT_NOFILE, &lim); //Max file descripter number
+     getrlimit(RLIMIT_NPROC, &lim); //Max number of processes
+     getrlimit(RLIMIT_STACK, &lim); //max stack size
+    
+    printf("max stack size: %ld \n", lim.rlim_max);
+    printf("max process limit: %ld\n", lim.rlim_max);
+    printf("max file descriptors: %ld\n", lim.rlim_max);
     
 }
+
+
